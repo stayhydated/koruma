baf13ce828f685bd282af6a701cf7cd902bb60fc
stayhydated <56778463+stayhydated@users.noreply.github.com>
2025-12-30 12:55:21 -0500

    Update lib.rs

--- crates/koruma/src/lib.rs
+++ crates/koruma/src/lib.rs
@@ -1,11 +1,1 @@
-use es_fluent::EsFluent;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, EsFluent)]
-pub enum NumberValidation {
-    Range { min: i32, max: i32 },
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, EsFluent)]
-pub enum StringValidation {
-    Length { min: usize, max: usize },
-}
